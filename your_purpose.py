a6bd124505f61a2782d42f3ca6e8d7f0c2081995
Andrew <125866011+ArchAndrew@users.noreply.github.com>
2024-09-30 00:29:51 -0400

    Add files via upload

---
+++ your_purpose.py
@@ -0,0 +1,138 @@
+#your_purpose.py
+
+print(''' 
+************************************************************************
+
+
+
+                              )
+                         )   __    (
+                        __  (~(    __
+                       (~(   \O\   )~)
+                        )O)   )_) (O(
+                       (_(__ (     )_) )
+                          )~)__      __
+                         /O/ )~)  ) (~(
+                        (_( (O(  __  \O\\
+                          )  )_)(~(   \_\\
+                         __      )O)   (             __________
+                 _      (~(   __(_(    __       ___-'          `-__
+          ,-----' |    _ \O\<'~_`)   ) )~)     / I'm rich beeotch! \\
+          | //  : |  -'   )_))^ \\  __(O(    __| MUHAHAHAHAHAH!!!    |
+          | //  : |   ---    >__;` (~( )_)   `-.  mmWAHAHAHA!!!!!   |
+          | //  : |  -._     /\_\   \O\         \    YEEHAH!!!     /
+          `-----._|     __  /__( \|  )_)         `--___________--'
+           _/___\_    //)_`/( (| ||]
+     _____[_______]_[~~-_ (.L)O) ||
+    [____________________]' (_(,/(~(
+      ||| /          )~)  ,___,'./\O\\
+      ||| \         (O(|,'______|( )_)
+      ||| /          )_) I==||  __
+      ||| \       __/_||  __||__)~)
+  -----||-/------`-._/||-o-_o__(O(--  __
+    ~~~~~'   ____     __  /_O_/.\_\   \~\\
+             \_O_\   /~/__/_/O`.o.     \O\\
+             ____   /O/_\_O/_/  `.' .   \_\\
+            /_O_/  /_/\_O_\ \O\   ,o,'
+                             \_\ `.'
+
+Thee_Architect_Was_Here
+************************************************************************
+''')
+print("Welcome to Your Purpose. ")
+print("Your mission is to get rich and flee Babylon with your money and your sanity. ")
+choice1 = input('You are at a crossroad, where do you want to go? '
+                 'Type "left" or "right".').lower()
+
+if choice1 == "left":
+    choice2 = print('''\n Ohh Noo!
+   .----------.
+      /  .-.  .-.  \\
+     /   | |  | |   \\
+     \   `-'  `-'  _/
+     /\     .--.  / |
+     \ |   /  /  / /
+     / |  `--'  /\ \\
+      /`-------'  \ \   ''')
+if choice1 == "right":
+    choice2 = input('\n You\'ve entered the sea of commerce '
+                    '\n Will you choose onsite corporate 9 to 5 or remote consultant?'
+                    '\n Type "onsite" for c-slave'
+                    '\n Type "remote" for consultant ') .lower()
+
+
+
+    
+if choice2 == "remote":
+    choice3 = input('\n You can now work any where in the country '
+                    '\n Now the key is to make it abroad'
+                    '\n You will have to find the key behind the door'
+                    '\n which door do you choose?'
+                    '\n red, yellow, or blue ')
+    if choice3 == "red":
+        print('''\n GAME OVER! You sold your soul to Satan. Here comes Diddy!
+                                                     ^             
+                                               / \           ^
+                         _,-~~~--~~~--._      (   \         / \\
+                     _,-'               `.__  (    \_.---._/   )
+                   ,'                       `-(_` -'       `-. )   
+                  /       "--..                \.'           `/  
+                 ,             `-.              :  _  .-.  _  : 
+                /                 ;             : (0).oYo.(0) ;
+              /                    `             \.-'V'"'V'-./
+             /                     '              \\^     ^//
+    /\      /                      '     :    : .-'\\^   ^//
+   ;  \    ;   /                  ,'  _.-`.    `. : \\^_^//
+   ;   \   ;  ;`.               ,'~~-'     `.    `.`.`-.-'
+    \   |_/   ;  `.        /-'/___.---.      `-.   `.`---.
+     \       /     |      /____.---.)))         `-. `---.\\
+      \_____/      (____________))))\\\            `-.\\\\
+                                 \\\\    ''')
+    elif choice3 == "yellow":
+        print('\n You got distracted, married corporate Klu Klux Keisha,' 
+              '\n She divorced you and took %50 of your sheeed and you\'ll never see your kids again.' 
+              '\n Tragic.')
+    elif choice3 == "blue":
+        print('''\n You found the key! \n You stayed focused. \n You increased your skill set got your certs, 401k, \n became a Cloud Security Consultant \n and now utilize your foreign trust and FEIE abroad
+              \n Blue Oceannn!
+              
+                                   _o
+                                  .#$?\\
+                                .'##$ ?$\\
+                              .'.###$  ?$$\\
+                            .' .####$   ?$$$\\
+                          .'  +#####$   `$$$$$\\
+                        .'   d######$    `$$$$$$\\
+                      .'   .########$     ?$$$$$$$\\
+                    .'    .#########$      ?$$$$$$$$\\
+                  .'     ,##########$       ?$$$$$$$$$\\
+                .'      +###########$       `?$$$$$$$$$$\\
+             ..'      .;############$        `$$$$$$$$$$$$\\
+             |'       ;    ````""""?$         ?$$$$??"""?$$$\\
+             |_/_____;._______._____$_________)?;__________?;X\___.-.
+             /'_____;_|______,|ooooo$oo|o,,,,/..|..___      |`'+\.\.'
+          .'||  ,ooooo|ooooodS|SSSSSSSS|SSS/'SSS|SSSSSSSSSSS|SSSS|/
+      .  .; |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/'
+.,;.;';`'(`;|',.;'.'.,,,+','+.,'+,.+,'+..^+,','+.,',.';.'.,+'`;'.';,:'';,:.'
+;'.';,:'.,;.;';`'(`;|',.;'.'.,,,+','+.,'+,.+,'+..^+,','+.,',.';.'.,+'`.';,:''')
+    else:
+        print("This door does not exist, try again.")
+else:
+    print('''\n GAME OVER! You sold your soul to Satan. Here comes Diddy!
+                                       ^             
+                                               / \           ^
+                         _,-~~~--~~~--._      (   \         / \\
+                     _,-'               `.__  (    \_.---._/   )
+                   ,'                       `-(_` -'       `-. )   
+                  /       "--..                \.'           `/  
+                 ,             `-.              :  _  .-.  _  : 
+                /                 ;             : (0).oYo.(0) ;
+              /                    `             \.-'V'"'V'-./
+             /                     '              \\^     ^//
+    /\      /                      '     :    : .-'\\^   ^//
+   ;  \    ;   /                  ,'  _.-`.    `. : \\^_^//
+   ;   \   ;  ;`.               ,'~~-'     `.    `.`.`-.-'
+    \   |_/   ;  `.        /-'/___.---.      `-.   `.`---.
+     \       /     |      /____.---.)))         `-. `---.\\
+      \_____/      (____________))))\\\            `-.\\\\
+                                 \\\\    ''')
